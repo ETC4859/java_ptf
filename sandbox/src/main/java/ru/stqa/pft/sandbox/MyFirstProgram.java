be2a2c0d9e7de9f59a38d175fe6e6c24ec89eef3
ETC4859 <47255703+ETC4859@users.noreply.github.com>
2019-02-07 23:12:40 +0300

    Задание №2

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,31 +1,17 @@
 package ru.stqa.pft.sandbox;
 
-public class MyFirstProgram
-{
 
-	public static void main(String[] args)	{
-		hello("world");
-		hello("user");
-		hello("PPC");
+public class MyFirstProgram {
 
-		double l = 5;
-		System.out.println ("площать квадрата со стороной " + l + " = " + area(l));
+  public static void main(String[] args) {
 
-		double a = 4;
-		double b = 6;
-		System.out.println ("площать прямоугольника со сторонами  " + a + " и " + b + " = " + area(a, b));
-			}
+    Point p1 = new Point(0, 0);
 
-	public static void hello(String somebody) {
+    Point p2 = new Point(3, 4);
 
-		System.out.println("Hello, " + somebody + "!");
-	}
+    double l = p1.distance(p1, p2);
 
-	public static double area (double len) {
-		return	len * len;
-	}
+    System.out.println("Расстояние между двумя точками = " + l);
 
-	public static double area (double a, double b) {
-		return a * b;
-	}
+  }
 }
